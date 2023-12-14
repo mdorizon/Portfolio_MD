09d09354a1f5a2caef603fe6b89dfbae9454eff1
mdorizon <mdorizon17@gmail.com>
2023-12-14 12:09:49 +0100

    text-copy

--- script.js
+++ script.js
@@ -3,8 +3,8 @@
 
 
 
-const mailCopy = document.querySelectorAll('.mail-copy');
-mailCopy.forEach((copy) => {
+const textCopy = document.querySelectorAll('.text-copy');
+textCopy.forEach((copy) => {
   copy.addEventListener('click', () => {
     const emailAddress = copy.textContent.trim();
     navigator.clipboard.writeText(emailAddress)
