68042c0995095a42b485b249413e672243cc07df
mdorizon <mdorizon17@gmail.com>
2023-12-14 12:06:44 +0100

    textcopy modified

--- script.js
+++ script.js
@@ -6,8 +6,8 @@
 const mailCopy = document.querySelectorAll('.mail-copy');
 mailCopy.forEach((copy) => {
   copy.addEventListener('click', () => {
-    if (copy.classList.contains('mdorizon17@gmail.com')) {
-      navigator.clipboard.writeText(copyText);
-    }
+    const emailAddress = copy.textContent.trim();
+    navigator.clipboard.writeText(emailAddress)
+
   })
 })
