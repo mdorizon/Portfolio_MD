5c4fded8bffef600ca90711fafb4bcd4882e54f7
Maxence <mdorizon17@gmail.com>
2023-12-17 16:30:05 +0100

    js sorting script

--- script.js
+++ script.js
@@ -1,12 +1,56 @@
-
-
-
-
-
+// TEXT COPY
 const textCopy = document.querySelectorAll('.text-copy');
 textCopy.forEach((copy) => {
   copy.addEventListener('click', () => {
     const emailAddress = copy.textContent.trim();
-    navigator.clipboard.writeText(emailAddress)
+    navigator.clipboard.writeText(emailAddress);
+  })
+})
+
+// PROJECT SORTING
+const sortButtons = document.querySelectorAll('.sort-button');
+const cards = document.querySelectorAll('.card')
+cards.forEach((card) => {
+  card.classList.add('d-none')
+  for (let i = 0; i < 3; i++) {
+    const card = cards[i];
+    card.classList.remove('d-none')
+  }
+})
+sortButtons.forEach((button) => {
+  button.addEventListener('click', () => {
+    // changing class active sort buttons
+    const buttonActive = document.querySelector('.sort-button.active');
+    buttonActive.classList.remove('active');
+    button.classList.add('active');
+
+    // sorting cards
+    switch (button.id) {
+      case 'all':
+        cards.forEach((card) => {
+          card.classList.add('d-none')
+          for (let i = 0; i < 3; i++) {
+            const card = cards[i];
+            card.classList.remove('d-none')
+          }
+        })
+        break;
+      case 'integration':
+        cards.forEach((card) => {
+          card.classList.remove('d-none')
+          if (!card.classList.contains('integration')) {
+            card.classList.add('d-none')
+          }
+        })
+        break;
+      case 'webdesign':
+        cards.forEach((card) => {
+          card.classList.remove('d-none')
+          if (!card.classList.contains('webdesign')) {
+            card.classList.add('d-none')
+          }
+        })
+        break;
+    }
   })
 })
